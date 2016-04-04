96c1feb31dd7f52a547eef5a9e6ee67b78cbdefa
Nicola Gruhl <ngruhl@welfenlab.de>
2016-04-04 11:06:21 +0200

    (feature) Added function to skip ports

---
+++ src/walkPort.js
@@ -0,0 +1,97 @@
+
+import _ from 'lodash'
+
+export function successor (graph, node, port) {
+  var edges = graph.nodeEdges(node)
+  var nodes = _.filter(edges, (e) => e.w === node + '_PORT_' + port).map((e) => e.w)
+  for (var i = 0; i < nodes.length; i++) {
+    while (graph.node(nodes[i])['nodeType'] !== 'process') {
+      var successors = graph.successors(nodes[i])
+      nodes[i] = successors[0]
+      nodes = nodes.concat(successors.slice(1, successors.length))
+    }
+  }
+  return nodes
+}
+
+export function predecessor (graph, node, port) {
+  var edges = graph.nodeEdges(node)
+  var nodes = _.filter(edges, (e) => e.v === node + '_PORT_' + port).map((e) => e.v)
+  for (var i = 0; i < nodes.length; i++) {
+    while (graph.node(nodes[i])['nodeType'] !== 'process') {
+      var predecessors = graph.predecessors(nodes[i])
+      nodes[i] = predecessors[0]
+      nodes = nodes.concat(predecessors.slice(1, predecessors.length))
+    }
+  }
+  return nodes
+}
+
+export function walk (graph, node, path) {
+  return generalWalk(graph, node, path, successor)
+}
+
+/**
+ * returns all pathes tracked by the path that defines the ports.
+ * The path will be pointing to node (node will be the last item of the result)
+ * it follows the direction of the directed edges
+ */
+export function walkBack (graph, node, path) {
+  return _.map(generalWalk(graph, node, path, predecessor), _.reverse)
+}
+
+/**
+ * returns a list of adjacent nodes for one port of a node
+ */
+export function adjacentNode (graph, node, port, edgeFollow) {
+  var adjacents = edgeFollow(graph, node, port)
+  if (adjacents.length === 0) return
+  else return adjacents
+}
+
+/**
+ * returns a list of adjacent of a node
+ */
+export function adjacentNodes (graph, node, ports, edgeFollow) {
+  if (!Array.isArray(ports)) {
+    ports = [ports]
+  }
+  var nodes = _.compact(_.map(ports, _.partial(adjacentNode, graph, node, _, edgeFollow)))
+  if (nodes.length === 0) return
+  return nodes
+}
+
+function generalWalk (graph, node, path, edgeFollow) {
+  if (Array.isArray(path)) {
+    return arrayWalk(graph, node, path, edgeFollow)
+  } else if (typeof (path) === 'function') {
+    return functionWalk(graph, node, path, edgeFollow)
+  } else {
+    return undefined
+  }
+}
+
+function functionWalk (graph, node, pathFn, edgeFollow) {
+  var followPorts = pathFn(graph, node)
+  if (!followPorts) {
+    return [node]
+  }
+  var nextNodes = adjacentNodes(graph, node, followPorts, edgeFollow)
+  var paths = _.compact(_.map(nextNodes, (pred) => functionWalk(graph, pred, pathFn, edgeFollow)))
+  return _.map(paths, (path) => _.flatten(_.concat([node], path)))
+}
+
+function arrayWalk (graph, node, pathArray, edgeFollow) {
+  return _.reduce(pathArray, (nodes, p) => {
+    return _(nodes)
+      .map((path) => {
+        var curNode = _.last(path)
+        var nextNodes = adjacentNodes(graph, curNode, p, edgeFollow)
+        if (!nextNodes) return
+        return _.map(nextNodes, (n) => _.concat(path, n))
+      })
+      .flatten()
+      .compact()
+      .value()
+  }, [[node]])
+}
---
+++ test/walkPort.js
@@ -0,0 +1,130 @@
+/* global describe, it */
+
+import chai from 'chai'
+import * as walk from '../src/walkPort.js'
+import grlib from 'graphlib'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+import fs from 'fs'
+import _ from 'lodash'
+
+var expect = chai.expect
+chai.use(sinonChai)
+
+describe('Adjacent nodes', () => {
+  var pGraph1 = grlib.json.read(JSON.parse(fs.readFileSync('./test/fixtures/testgraph0_generics.graphlib')))
+  it('can get the predecessor of a node', () => {
+    var pred = walk.predecessor(pGraph1, '3_STDOUT', 'input')
+    expect(pred).to.deep.equal(['2_DEMUX'])
+  })
+
+  it('can get the successor of a node', () => {
+    var pred = walk.successor(pGraph1, '0_CONST1', 'const1')
+    expect(pred).to.deep.equal(['2_DEMUX'])
+  })
+
+  var doubleInGraph = grlib.json.read(JSON.parse(fs.readFileSync('./test/fixtures/graph_double_in.graphlib')))
+  it('can get multiple predecessors from one port', () => {
+    var pred = walk.predecessor(doubleInGraph, '2_STDOUT', 'input')
+    expect(pred).to.have.length(2)
+    expect(pred).to.include('0_STDIN')
+    expect(pred).to.include('1_STDIN')
+  })
+
+  var doubleOutGraph = grlib.json.read(JSON.parse(fs.readFileSync('./test/fixtures/graph_double_out.graphlib')))
+  it('can get multiple successor from one port', () => {
+    var pred = walk.successor(doubleOutGraph, '0_STDIN', 'output')
+    expect(pred).to.have.length(2)
+    expect(pred).to.include('1_STDOUT')
+    expect(pred).to.include('2_STDOUT')
+  })
+
+  it('`adjacentNode` returns edgeFollow result', () => {
+    var pred = walk.adjacentNode(pGraph1, '0_CONST1', 'const1', () => ['NEXT_NODE'])
+    expect(pred).to.deep.equal(['NEXT_NODE'])
+  })
+
+  it('`adjacentNode` can use successor function', () => {
+    var succ = walk.adjacentNode(pGraph1, '0_CONST1', 'const1', walk.successor)
+    expect(succ).to.deep.equal(['2_DEMUX'])
+  })
+
+  it('`adjacentNode` returns undefined if path does not exists', () => {
+    var succ = walk.adjacentNode(pGraph1, '0_CONST1', 'a', walk.successor)
+    expect(succ).to.be.undefined
+  })
+
+  it('`adjacentNodes` can process multiple ports', () => {
+    var preds = walk.adjacentNodes(pGraph1, '2_DEMUX', ['input', 'control'], walk.predecessor)
+    expect(preds).to.have.length(2)
+    expect(preds[0]).to.have.length(1)
+    expect(preds[1]).to.have.length(1)
+    expect(_.flatten(preds)).to.include('0_CONST1')
+    expect(_.flatten(preds)).to.include('1_CONST2')
+  })
+
+  it('`adjacentNodes` removes not usable paths', () => {
+    var preds = walk.adjacentNodes(pGraph1, '2_DEMUX', ['input', '-'], walk.predecessor)
+    expect(preds).to.have.length(1)
+    expect(preds[0]).to.deep.equal(['0_CONST1'])
+  })
+})
+
+describe('Graph walks', () => {
+  var pGraph1 = grlib.json.read(JSON.parse(fs.readFileSync('./test/fixtures/testgraph0_generics.graphlib')))
+  it('can walk forward through a given path', () => {
+    var path = walk.walk(pGraph1, '0_CONST1', ['const1', 'outTrue'])
+    expect(path).to.have.length(1)
+    expect(path[0]).to.deep.equal(['0_CONST1', '2_DEMUX', '3_STDOUT'])
+  })
+
+  it('can walk backward through a given path', () => {
+    var path = walk.walkBack(pGraph1, '3_STDOUT', ['input', 'control'])
+    expect(path).to.have.length(1)
+    expect(path[0]).to.deep.equal(['1_CONST2', '2_DEMUX', '3_STDOUT'])
+  })
+
+  it('returns empty array if the path does not exist', () => {
+    var path = walk.walk(pGraph1, '0_CONST1', ['a'])
+    expect(path).to.have.length(0)
+  })
+
+  it('can follow multiple paths', () => {
+    var path = walk.walkBack(pGraph1, '2_DEMUX', [['control', 'input']])
+    expect(path).to.have.length(2)
+    expect(path[0][0]).to.be.oneOf(['0_CONST1', '1_CONST2'])
+    expect(path[1][0]).to.be.oneOf(['0_CONST1', '1_CONST2'])
+  })
+
+  it('can follow a path given by a function', () => {
+    var cb = sinon.stub()
+    cb.onCall(0).returns('const1')
+    cb.onCall(1).returns('outFalse')
+    var path = walk.walk(pGraph1, '0_CONST1', cb)
+    expect(path).to.have.length(1)
+    expect(path[0]).to.deep.equal(['0_CONST1', '2_DEMUX', '4_STDOUT'])
+  })
+
+  it('returns an empty array if the path given by a function does not exist', () => {
+    var cb = sinon.stub()
+    cb.onCall(0).returns('a')
+    var path = walk.walk(pGraph1, '0_CONST1', cb)
+    expect(path).to.have.length(0)
+  })
+
+  it('gives a valid node object to the path function', () => {
+    walk.walk(pGraph1, '0_CONST1', (graph, node) => {
+      expect(graph).to.be.ok
+      expect(node).to.be.a('string')
+    })
+  })
+
+  it('can follow multiple paths via a function', () => {
+    var cb = sinon.stub()
+    cb.onCall(0).returns(['input', 'control'])
+    var path = walk.walkBack(pGraph1, '2_DEMUX', cb)
+    expect(path).to.have.length(2)
+    expect(path[0][0]).to.be.oneOf(['0_CONST1', '1_CONST2'])
+    expect(path[1][0]).to.be.oneOf(['0_CONST1', '1_CONST2'])
+  })
+})
