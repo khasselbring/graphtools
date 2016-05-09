c9b476dd2fe6010f1cc81dbab28a413a8743c318
Georg Schuppe <georg.schuppe@gmail.com>
2016-05-09 11:13:02 +0200

    (fix) added compoundify to the api

--- src/api.js
+++ src/api.js
@@ -2,5 +2,6 @@
 import * as walk from './walk'
 import * as rewrite from './rewrites'
 import * as utils from './utils'
+import * as compoundify from './compoundify'
 
-export {walk as walk, rewrite as rewrite, utils as utils}
+export {walk as walk, rewrite as rewrite, utils as utils, compoundify as compoundify}
