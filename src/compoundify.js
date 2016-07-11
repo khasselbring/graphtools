03c0dbf1516f2a52753908d0179aaf198b935eeb
Maximilian Klein <dasmaxl@gmail.com>
2016-07-11 14:04:11 +0200

    (cleanup) Remove console.logs and make linter happy.

--- src/compoundify.js
+++ src/compoundify.js
@@ -2,7 +2,6 @@ import graphlib from 'graphlib'
 import _ from 'lodash'
 import hash from 'object-hash'
 import { utils, graph as graphAPI } from './api'
-import fs from 'fs'
 
 var markNodes = function (graph, subset) {
   for (let n of subset) {
@@ -101,7 +100,6 @@ export function isCompoundable (g, subset) {
         continue
       }
       if (blockedForward(topsort[i], graph, topsort, last) && blockedBackward(topsort[i], graph, topsort)) {
-        console.error('blocked', topsort[i])
         return false
       }
     }
@@ -114,7 +112,7 @@ export function compoundify (g, subset, name, label) {
   var origSubset = _.clone(subset)
   if (utils.isNG(g)) { subset = completeSubset(g, subset) }
   if (!isCompoundable(g, subset)) {
-    throw new Error('This subset cannot be compoundified given this particular subset.' + JSON.stringify(origSubset)) 
+    throw new Error('This subset cannot be compoundified given this particular subset.' + JSON.stringify(origSubset))
   }
   var graph = graphAPI.clone(g)
   if (!name) {
