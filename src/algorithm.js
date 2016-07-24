0c9ba77d435f1acc69573def54cd7cc73ca96794
Maximilian Klein <dasmaxl@gmail.com>
2016-07-24 22:42:44 +0200

    (cleanup) Remove old way of calculating topological sorting.

--- src/algorithm.js
+++ src/algorithm.js
@@ -3,38 +3,23 @@
  */
 
 import graphlib from 'graphlib'
-import {clone} from './graph'
-import * as utils from './utils'
 import fs from 'fs'
-import _ from 'lodash'
+import debugLog from 'debug'
+
+const debug = debugLog('graphtools')
 
 /**
- * Removes all continuations from a graph (only for debug purposes)
- * @param {Graphlib} graph The graph
- * @returns {Graphlib} A graph that has no continuations edges
- */
-export function removeContinuations (graph) {
-  var tGraph = clone(graph)
-  _.each(tGraph.edges(), (e) => {
-    if (utils.isContinuation(tGraph, e)) {
-      tGraph.removeEdge(e)
-    }
-  })
-  return tGraph
-}
-
-/**
- * Returns a topological sorting of the graph. Removes all continuations before calculating the topological sorting.
- * @param {Graphlib} graph The graph.
- * @return {string[]} A sorting of the nodes.
+ * Returns a topological sorting of the graph.
+ * @param {PortGraph} graph The graph.
+ * @return {string[]} A sorting of the nodes given by an array of their IDs.
  * @throws {Error} If the graph has loops.
  */
 export function topologicalSort (graph) {
   try {
-    var tGraph = removeContinuations(graph)
-    return graphlib.alg.topsort(tGraph)
+    return null // TODO
   } catch (err) {
-    fs.writeFileSync('test.json', JSON.stringify(graphlib.json.write(tGraph)))
-    throw Error('[topoSort] Cannot calculate toplogical sorting, graph contains loop.\n' + JSON.stringify(graphlib.alg.findCycles(tGraph)))
+    debug(JSON.stringify(graph, null, 2)) // make printing the graph possible
+    fs.writeFileSync('test.json', JSON.stringify(graphlib.json.write(graph)))
+    throw Error('[topoSort] Cannot calculate toplogical sorting, graph contains loop.\n' + JSON.stringify(graphlib.alg.findCycles(graph)))
   }
 }
