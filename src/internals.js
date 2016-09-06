0d1f561ef1026c0ef7a5d11724bf02d77f51e70c
Maximilian Klein <dasmaxl@gmail.com>
2016-09-06 09:53:01 +0200

    (fix) Use package version and not version in execution path.

--- src/internals.js
+++ src/internals.js
@@ -1,7 +1,8 @@
 
 import fs from 'fs'
+import {join} from 'path'
 
-const version = JSON.parse(fs.readFileSync('./package.json')).version
+const version = JSON.parse(fs.readFileSync(join(__dirname, '../package.json'))).version
 
 export function packageVersion () {
   return version
