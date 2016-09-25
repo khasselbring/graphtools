c077b5e8db84e80d4cc8cfb5da5318941f5a6830
Maximilian Klein <mklein@welfenlab.de>
2016-09-25 17:09:23 +0200

    (feature) Include predecessor into compound correctly.

--- src/rewrite/compound.js
+++ src/rewrite/compound.js
@@ -3,15 +3,30 @@
  */
 
 import curry from 'lodash/fp/curry'
+import any from 'lodash/fp/any'
+import negate from 'lodash/fp/negate'
 import {chain} from '../graph/chain'
 import * as Compound from '../compound'
-import {predecessor, inIncidents, outIncidents} from '../graph/connections'
+import {predecessor, predecessors, inIncidents, outIncidents} from '../graph/connections'
 import * as Graph from '../graph'
+import * as Node from '../node'
 import {mergeNodes} from '../graph/internal'
 
+/**
+ * Moves the predecessor of a port into the compound node. It changes the signature of the
+ * compound node. It has to ensure that the inputs are correct. This only works if the predecessor has
+ * only one successor (i.e. the compound node it will move into).
+ * @params {Port} port A port identifier. This also specifies the node.
+ * @params {PortGraph} graph The graph in which the change will happen.
+ * @returns {PortGraph} A new port graph that includes the predecessor of the port in the compound.
+ * @throws {Error} If the predecessor has more than one successor.
+ */
 export const includePredecessor = curry((port, graph) => {
   var pred = predecessor(port, graph)
   var predNode = Graph.node(pred, graph)
+  if (outIncidents(pred.node, graph).length > 1) {
+    throw new Error('Cannot include the predecessor of port: ' + JSON.stringify(port) + ' as it has multiple successors.')
+  }
   var preInPorts = inIncidents(pred.node, graph)
   var postInPorts = outIncidents(port, graph)
   var compound = Graph.node(port, graph)
@@ -40,4 +55,23 @@ export const includePredecessor = curry((port, graph) => {
         Graph.addEdge({from: edge.from, to: compound.id + '@' + edge.to.port})))
   )(graph)
   return newGraph
+})
+
+/**
+ * Moves a node from its compound to the parent compound node. Changes the compound node to
+ * ensure it takes the correct number of inputs etc. This method only works if the node has no
+ * predecessor in the compound node.
+ * @params {Node} node A node identifier for the node that should be moved out of the compound node.
+ * @params {PortGraph} graph The graph
+ * @returns {PortGraph} A new graph in which the node is moved out of its parent compound into the parent
+ * of its parent.
+ * @throws {Error} If the node has a predecessor in the compound and thus cannot be moved out of the
+ * compound node.
+ */
+export const excludeNode = curry((node, graph) => {
+  var parent = Graph.parent(node, graph)
+  var preds = predecessors(node, graph)
+  if (any(negate(Node.equal(parent)), preds)) {
+    throw new Error('Node has predecessor in the parent compound and thus cannot be moved out of the compound node.')
+  }
 })
--- test/rewrite.js
+++ test/rewrite.js
@@ -7,24 +7,65 @@ import {includePredecessor} from '../src/rewrite/compound'
 var expect = chai.expect
 
 describe.only('Rewrite basic API', () => {
-  it('can include the direct predecessor of a compound port into the compound', () => {
-    var comp = Graph.addEdge({from: '@inC', to: '@outC'},
-      Graph.compound({name: 'c', ports: [{port: 'inC', kind: 'input'}, {port: 'outC', kind: 'output'}]}))
-    var graph = Graph.chain(
-      Graph.addNode({ports: [{port: 'outA', kind: 'output'}, {port: 'inA', kind: 'input'}], componentId: 'moved'}),
-      Graph.addNode(comp),
-      Graph.addNode({ports: [{port: 'outF', kind: 'output'}]}),
-      (graph, objs) =>
-        Graph.addEdge({from: objs()[0].id + '@outA', to: objs()[1].id + '@inC'})(graph),
-      (graph, objs) =>
-        Graph.addEdge({from: objs()[2].id + '@outF', to: objs()[0].id + '@inA'})(graph)
-    )()
-    expect(Graph.nodes(graph)).to.have.length(3)
-    expect(Graph.nodes(Graph.node('c', graph))).to.have.length(0)
-    var rewGraph = includePredecessor({node: 'c', port: 'inC'}, graph)
-    expect(Graph.nodes(rewGraph)).to.have.length(2)
-    expect(Graph.nodes(Graph.node('c', rewGraph))).to.have.length(1)
-    expect(Graph.inIncidents('c', rewGraph)).to.have.length(2)
-    expect(Graph.node(Graph.predecessor('c@outC', rewGraph), rewGraph).componentId).to.equal('moved')
+  describe('Including predecessors', () => {
+    it('can include the direct predecessor of a compound port into the compound', () => {
+      var comp = Graph.addEdge({from: '@inC', to: '@outC'},
+        Graph.compound({name: 'c', ports: [{port: 'inC', kind: 'input'}, {port: 'outC', kind: 'output'}]}))
+      var graph = Graph.chain(
+        Graph.addNode({ports: [{port: 'outA', kind: 'output'}, {port: 'inA', kind: 'input'}], componentId: 'moved'}),
+        Graph.addNode(comp),
+        Graph.addNode({ports: [{port: 'outF', kind: 'output'}]}),
+        (graph, objs) =>
+          Graph.addEdge({from: objs()[0].id + '@outA', to: objs()[1].id + '@inC'})(graph),
+        (graph, objs) =>
+          Graph.addEdge({from: objs()[2].id + '@outF', to: objs()[0].id + '@inA'})(graph)
+      )()
+      expect(Graph.nodes(graph)).to.have.length(3)
+      expect(Graph.nodes(Graph.node('c', graph))).to.have.length(0)
+      var rewGraph = includePredecessor({node: 'c', port: 'inC'}, graph)
+      expect(Graph.nodes(rewGraph)).to.have.length(2)
+      expect(Graph.nodes(Graph.node('c', rewGraph))).to.have.length(1)
+      expect(Graph.inIncidents('c', rewGraph)).to.have.length(2)
+      expect(Graph.node(Graph.predecessor('c@outC', rewGraph), rewGraph).componentId).to.equal('moved')
+    })
+
+    it('can include the direct predecessor via short notation', () => {
+      var comp = Graph.addEdge({from: '@inC', to: '@outC'},
+        Graph.compound({name: 'c', ports: [{port: 'inC', kind: 'input'}, {port: 'outC', kind: 'output'}]}))
+      var graph = Graph.chain(
+        Graph.addNode({ports: [{port: 'outA', kind: 'output'}, {port: 'inA', kind: 'input'}], componentId: 'moved'}),
+        Graph.addNode(comp),
+        Graph.addNode({ports: [{port: 'outF', kind: 'output'}]}),
+        (graph, objs) =>
+          Graph.addEdge({from: objs()[0].id + '@outA', to: objs()[1].id + '@inC'})(graph),
+        (graph, objs) =>
+          Graph.addEdge({from: objs()[2].id + '@outF', to: objs()[0].id + '@inA'})(graph)
+      )()
+      expect(Graph.nodes(graph)).to.have.length(3)
+      expect(Graph.nodes(Graph.node('c', graph))).to.have.length(0)
+      var rewGraph = includePredecessor('c@inC', graph)
+      expect(Graph.nodes(rewGraph)).to.have.length(2)
+      expect(Graph.nodes(Graph.node('c', rewGraph))).to.have.length(1)
+      expect(Graph.inIncidents('c', rewGraph)).to.have.length(2)
+      expect(Graph.node(Graph.predecessor('c@outC', rewGraph), rewGraph).componentId).to.equal('moved')
+    })
+
+    it('throws an error if the predecessor of an port has other successors', () => {
+      var comp = Graph.addEdge({from: '@inC', to: '@outC'},
+        Graph.compound({name: 'c', ports: [{port: 'inC', kind: 'input'}, {port: 'outC', kind: 'output'}]}))
+      var graph = Graph.chain(
+        Graph.addNode({ports: [{port: 'outA', kind: 'output'}, {port: 'inA', kind: 'input'}], componentId: 'moved'}),
+        Graph.addNode(comp),
+        Graph.addNode({ports: [{port: 'outF', kind: 'output'}]}),
+        Graph.addNode({ports: [{port: 'inB', kind: 'input'}]}),
+        (graph, objs) =>
+          Graph.addEdge({from: objs()[0].id + '@outA', to: objs()[1].id + '@inC'})(graph),
+        (graph, objs) =>
+          Graph.addEdge({from: objs()[0].id + '@outA', to: objs()[3].id + '@inB'})(graph),
+        (graph, objs) =>
+          Graph.addEdge({from: objs()[2].id + '@outF', to: objs()[0].id + '@inA'})(graph)
+      )()
+      expect(() => includePredecessor({node: 'c', port: 'inC'}, graph)).to.throw(Error)
+    })
   })
 })
