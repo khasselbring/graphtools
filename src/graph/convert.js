44acab27d5a18efb05c5625c338277e43b50cd2e
Alexander Treptau <atreptau@welfenlab.de>
2016-10-28 18:31:12 +0200

    (fix) Add missing data to fromJSON, update tests.

--- src/graph/convert.js
+++ src/graph/convert.js
@@ -1,5 +1,5 @@
 
-// import concat from 'lodash/fp/concat'
+import concat from 'lodash/fp/concat'
 import cloneDeep from 'lodash/fp/cloneDeep'
 // import {empty} from './basic'
 // import {addNode, addNodeTuple} from './node'
@@ -14,7 +14,12 @@ import cloneDeep from 'lodash/fp/cloneDeep'
  * @returns {PortGraph} The port graph with its functions.
  */
 export function fromJSON (jsonGraph) {
-  return cloneDeep(jsonGraph)
+  var graph = cloneDeep(jsonGraph)
+  if (!graph.path) { graph.path = [] }
+  if (!graph.nodes) { graph.nodes = concat(jsonGraph.Nodes || [], (Array.isArray(jsonGraph.nodes)) ? jsonGraph.nodes : []) }
+  if (!graph.edges) { graph.edges = concat(jsonGraph.Edges || [], (Array.isArray(jsonGraph.edges)) ? jsonGraph.edges : []) }
+  if (!graph.components) { graph.components = concat(jsonGraph.Components || [], (Array.isArray(jsonGraph.components)) ? jsonGraph.components : []) }
+  return graph
   // TODO: add checks back in again
   // var nodes = concat(jsonGraph.Nodes || [], (Array.isArray(jsonGraph.nodes)) ? jsonGraph.nodes : [])
   // var edges = concat(jsonGraph.Edges || [], (Array.isArray(jsonGraph.edges)) ? jsonGraph.edges : [])
--- test/graph.js
+++ test/graph.js
@@ -633,68 +633,75 @@ describe('Basic graph functions', () => {
     })
   })
 
-  describe('Wip temp tests', () => {
-
-    // it('Compound addEdge ...', () => {
-    //   var comp = Graph.addEdge({from: '@inC', to: '@outC'},
-    //     Graph.compound({name: 'c', ports: [{port: 'inC', kind: 'input'}, {port: 'outC', kind: 'output'}]}))
-    //   expect(Graph.predecessors('', comp)).to.have.length(1)
-    //   expect(Graph.successors('', comp)).to.have.length(1)
-    //   var graph = Graph.addNode(comp, Graph.empty())
-    //   expect(Graph.predecessors('c', graph)).to.have.length(1)
-    //   expect(Graph.successors('c', graph)).to.have.length(1)
-
-    //   console.log(Graph.toJSON(comp))
-    //   console.log(Graph.toJSON(graph))
-    // })
-
-    it('Compound addEdge', () => {
-      var graphJSON = {
+  describe('Wip temp tests', () => { // TODO: rename
+    var graphJSON = {
         nodes:
         [{
           ref: 'math/add',
-          id: '#ciujt9ktl0003lumriay99r9m',
-          settings: {},
-          ports: []
+          id: '#ciujt9ktl0003lumriay99r9m'
         },
         {
           ref: 'std/const',
-          MetaInformation: [Object],
-          id: '#ciujt9ktp0004lumrth5o5vnd',
-          settings: {},
-          ports: []
+          id: '#ciujt9ktp0004lumrth5o5vnd'
         }],
-        metaInformation: {},
-        edges: [],
-        components: [],
         ports:
-        [{ name: 'x', kind: 'input', type: 'generic' },
-        { name: 'value', kind: 'output', type: 'generic' }],
+          [{ port: 'x', kind: 'input', type: 'generic' },
+           { port: 'value', kind: 'output', type: 'generic' }],
         atomic: false,
         id: '#ciujt9kti0002lumr7poczkzx',
         version: '0.0.0',
-        componentId: 'myInc'
+        componentId: 'myInc',
+        name: 'c'
       }
+
+    it('import', () => {
       var graph = Graph.fromJSON(graphJSON)
       expect(graph).to.be.ok
       expect(Graph.nodes(graph)).to.have.length(2)
       expect(Graph.edges(graph)).to.have.length(0)
       expect(Graph.components(graph)).to.have.length(0)
       expect(graph.ports).to.be.deep.equal(graphJSON.ports)
+    })
 
+    it('Simple add edge test', () => {
+      var comp = Graph.addEdge({from: '@x', to: '@value'}, Graph.compound(graphJSON))
+      expect(comp).to.be.ok
+
+      expect(Graph.predecessors('', comp)).to.have.length(1)
+      expect(Graph.successors('', comp)).to.have.length(1)
+      var graph = Graph.addNode(comp, Graph.empty())
+      expect(Graph.predecessors('c', graph)).to.have.length(1)
+      expect(Graph.successors('c', graph)).to.have.length(1)
+    })
+
+    it('Use addEdge @name notation', () => {
+      var graph = Graph.fromJSON(graphJSON)
       var cmpt = Graph.compound(graphJSON)
-      var newGraphA = Graph.addEdge({from: '@x', to: '@value'}, cmpt)
-      var newGraphB = Graph.addEdge({from: '@x', to: '@value'}, graph)
+      var out = Graph.addEdge({from: '@x', to: '@value'}, cmpt)
+      var out2 = Graph.addEdge({from: '@x', to: '@value'}, graph)
+      expect(out).to.be.ok
+      expect(out2).to.be.ok
+    })
 
-      expect(newGraphA).to.be.ok
-      expect(newGraphB).to.be.ok
+    it('Use addEdge @Number notation', () => {
+      var graph = Graph.fromJSON(graphJSON)
+      var out = Graph.addEdge({from: '@0', to: '@0'}, graph) // or should it be: {from: '@0', to: '@1'} ?
+      var out2 = Graph.addEdge({from: '@0', to: '#ciujt9ktl0003lumriay99r9m@0'}, graph)
+      expect(out).to.be.ok
+      expect(out2).to.be.ok
+    })
 
-      expect(Graph.nodes(newGraph)).to.have.length(2)
-      expect(Graph.edges(newGraph)).to.have.length(1)
-      expect(Graph.components(newGraph)).to.have.length(0)
+    it('Use addEdge #id@name notation', () => {
+      var out = Graph.addEdge({from: '#ciujt9kti0002lumr7poczkzx@x', to: '#ciujt9kti0002lumr7poczkzx@value'}, Graph.compound(graphJSON))
+      expect(out).to.be.ok
+    })
 
-
-      console.log(Graph.toJSON(newGraph))
+    it('Use addEdge #id@Number notation', () => {
+      var graph = Graph.fromJSON(graphJSON)
+      var out = Graph.addEdge({from: '#ciujt9kti0002lumr7poczkzx@0', to: '#ciujt9kti0002lumr7poczkzx@0'}, graph)
+      var out2 = Graph.addEdge({from: '#ciujt9kti0002lumr7poczkzx@0', to: '#ciujt9ktl0003lumriay99r9m@0'}, graph)
+      expect(out).to.be.ok
+      expect(out).to.be.ok
     })
   })
 })
